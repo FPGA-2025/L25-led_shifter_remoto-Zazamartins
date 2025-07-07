d29b9babcd5a355a09d54ded247ba60f1495f7fe
Zazamartins <isaacmrtnsss@gmail.com>
2025-07-07 15:06:55 -0300

    Ok

--- led_shifter.v
+++ led_shifter.v
@@ -6,5 +6,23 @@ module LedShifter #(
     output  reg [7:0] leds
 );
 
+    localparam QUARTER_SECOND = CLK_FREQ/4; 
+    reg [31:0] counter;
+
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            leds <= 8'b00011111;
+            counter <= 'h0000;
+        end
+        else begin
+            if (counter >= QUARTER_SECOND - 1)begin
+                counter <= 0;
+                leds <= {leds[6:0], leds[7]};
+            end
+            else begin
+                counter <= counter + 1;
+            end
+        end 
+    end
     
 endmodule
